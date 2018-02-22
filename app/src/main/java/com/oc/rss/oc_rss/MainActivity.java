f18eae19b86194ce999e1f847f6444f683e8937b
unknown <douangpaseut.willy@gmail.com>
2018-02-22 14:22:42 +0100

    added actionBar for MainActivity

--- app/src/main/java/com/oc/rss/oc_rss/MainActivity.java
+++ app/src/main/java/com/oc/rss/oc_rss/MainActivity.java
@@ -3,6 +3,8 @@ package com.oc.rss.oc_rss;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -35,5 +37,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_edit:
+                /* DO EDIT */
+                return true;
+            case R.id.action_add:
+                /* DO ADD */
+                return true;
+            case R.id.action_delete:
+                /* DO DELETE */
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
